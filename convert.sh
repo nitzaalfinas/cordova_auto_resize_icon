9b9dabe31dece05e2e9bccddb011546ab1ca663f
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-07-13 17:01:44 +0700

    anu
    itu

---
+++ convert.sh
@@ -0,0 +1,16 @@
+convert logo_qiriman.png -resize 40x40^ icon-40.png &&
+convert logo_qiriman.png -resize 80x80^ icon-40@2x.png &&
+convert logo_qiriman.png -resize 50x50^ icon-50.png &&
+convert logo_qiriman.png -resize 100x100^ icon-50@2x.png && 
+convert logo_qiriman.png -resize 120x120^ icon-60@2x.png &&
+convert logo_qiriman.png -resize 180x180^ icon-60@3x.png &&
+convert logo_qiriman.png -resize 72x72^ icon-72.png &&
+convert logo_qiriman.png -resize 144x144^ icon-72@2x.png &&
+convert logo_qiriman.png -resize 76x76^ icon-76.png &&
+convert logo_qiriman.png -resize 152x152^ icon-76@2x.png &&
+convert logo_qiriman.png -resize 29x29^ icon-small.png &&
+convert logo_qiriman.png -resize 58x58^ icon-small@2x.png &&
+convert logo_qiriman.png -resize 87x87^ icon-small@3x.png &&
+convert logo_qiriman.png -resize 57x57^ icon.png &&
+convert logo_qiriman.png -resize 114x114^ icon@2x.png &&
+convert logo_qiriman.png -resize 167x167^ icon-83.5@2x.png
