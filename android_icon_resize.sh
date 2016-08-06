30cbec8e0dcd3c97df9bb08d52a6b1a28906d422
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-08-06 10:10:41 +0700

    Create android_icon_resize.sh

---
+++ android_icon_resize.sh
@@ -0,0 +1,4 @@
+convert my_icon.png -resize 72x72^ drawable-hdpi/icon.png &&
+convert my_icon.png -resize 36x36^ drawable-ldpi/icon.png &&
+convert my_icon.png -resize 48x48^ drawable-mdpi/icon.png &&
+convert my_icon.png -resize 96x96^ drawable-xhdpi/icon.png
