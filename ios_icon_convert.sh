3a32ea4d9b5f55ebdf7a0ed504c8d439a7c40d2f
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-08-06 10:00:54 +0700

    Create ios_icon_convert.sh

---
+++ ios_icon_convert.sh
@@ -0,0 +1,16 @@
+convert my_icon.png -resize 40x40^ icon-40.png &&
+convert my_icon.png -resize 80x80^ icon-40@2x.png &&
+convert my_icon.png -resize 50x50^ icon-50.png &&
+convert my_icon.png -resize 100x100^ icon-50@2x.png && 
+convert my_icon.png -resize 120x120^ icon-60@2x.png &&
+convert my_icon.png -resize 180x180^ icon-60@3x.png &&
+convert my_icon.png -resize 72x72^ icon-72.png &&
+convert my_icon.png -resize 144x144^ icon-72@2x.png &&
+convert my_icon.png -resize 76x76^ icon-76.png &&
+convert my_icon.png -resize 152x152^ icon-76@2x.png &&
+convert my_icon.png -resize 29x29^ icon-small.png &&
+convert my_icon.png -resize 58x58^ icon-small@2x.png &&
+convert my_icon.png -resize 87x87^ icon-small@3x.png &&
+convert my_icon.png -resize 57x57^ icon.png &&
+convert my_icon.png -resize 114x114^ icon@2x.png &&
+convert my_icon.png -resize 167x167^ icon-83.5@2x.png
