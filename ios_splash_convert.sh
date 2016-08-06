a779a4ff202d6081f934b0e56e3f78ec3f379df0
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-08-06 09:58:47 +0700

    Create ios_splash_convert.sh

---
+++ ios_splash_convert.sh
@@ -0,0 +1,10 @@
+convert my_splash.png -resize 640x1136! Default-568h@2x~iphone.png && 
+convert my_splash.png -resize 750x1334! Default-667h.png && 
+convert my_splash.png -resize 1242x2208! Default-736h.png && 
+convert my_splash.png -resize 2208x1242! Default-Landscape-736h.png && 
+convert my_splash.png -resize 2048x1536! Default-Landscape@2x~ipad.png &&
+convert my_splash.png -resize 1024x768! Default-Landscape~ipad.png &&
+convert my_splash.png -resize 1536x2048! Default-Portrait@2x~ipad.png &&
+convert my_splash.png -resize 768x1024! Default-Portrait~ipad.png &&
+convert my_splash.png -resize 640x960! Default@2x~iphone.png &&
+convert my_splash.png -resize 320x480! Default~iphone.png
