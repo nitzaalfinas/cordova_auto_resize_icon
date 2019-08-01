2ecb1c5cfb17d1bf9d30700a16207de797d790c6
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-08-01 08:26:16 +0700

    Update android_icon_convert.sh

--- android_icon_convert.sh
+++ android_icon_convert.sh
@@ -1,4 +1,7 @@
-convert my_icon.png -resize 72x72^ drawable-hdpi/icon.png &&
-convert my_icon.png -resize 36x36^ drawable-ldpi/icon.png &&
-convert my_icon.png -resize 48x48^ drawable-mdpi/icon.png &&
-convert my_icon.png -resize 96x96^ drawable-xhdpi/icon.png
+convert my_icon.png -resize 72x72 drawable-hdpi-icon.png &&
+convert my_icon.png -resize 36x36 drawable-ldpi-icon.png &&
+convert my_icon.png -resize 48x48 drawable-mdpi-icon.png &&
+convert my_icon.png -resize 96x96 drawable-xhdpi-icon.png &&
+convert my_icon.png -resize 192x192 drawable-xxxhdpi-icon.png &&
+convert my_icon.png -resize 144x144 drawable-xxhdpi-icon.png &&
+convert my_icon.png -resize 192x192 ic_launcher.png
